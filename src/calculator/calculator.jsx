d31246a33ffc54438b549ebe049ff37c8aed140b
BhanuBurman <bhanuburman484@gmail.com>
2024-06-09 02:43:32 +0530

    Added Length Constraints and dynamic query design

--- src/calculator/calculator.jsx
+++ src/calculator/calculator.jsx
@@ -3,30 +3,66 @@ import './calculator.scss'
 const Calculator = () => {
     const [query, setQuery] = useState('00')
     const [result, setResult] = useState('null')
+    const [fontSize, setFontSize] = useState('45px')
+    const [color, setColor] = useState('white')
+    const style = {
+      "fontSize":fontSize, 
+      "color":color, 
+      "width": "100%",  
+      "wordWrap": "break-word",
+      "textAlign": "end",
+      "transition": 'all 0.3s ease'
+    }
+    
     const calculate = (e) => {
       // console.log(e);
       const value = e.target.innerText;
-       if(value== 'AC'){
-           setQuery('00')
-           setResult('null')
-       }else if(value === 'DEL'){
-           setQuery(query.slice(0, -1))
-       }else if(value === '='){
-           setResult(eval(query))
-       }else{
-           if(query === '00'){
-               setQuery(value)
-           }else{
-               setQuery(query + value)
-           }
-       }
+      if(color === 'gray') {
+        setColor('white');
+      }
+      if(result !== 'null'){
+        setResult('null');
+      }
+      if(query.length > 7 && query.length <= 12){
+        setFontSize('40px');
+      }
+      if(query.length > 12 && query.length <= 17){
+        setFontSize('35px');
+      }
+      if (query.length > 16){
+        setFontSize('30px');
+      }
+      if(value === 'DEL'){
+        setQuery(query.slice(0, -1))
+    }
+      else if(query.length > 30){
+        alert('Please enter smaller query!');
+      }else{
+        if(value== 'AC'){
+          setColor('white');
+          setFontSize('45px');
+          setQuery('00')
+          setResult('null')
+        }else if(value === '='){
+            setColor('gray');
+            setFontSize('20px');
+            setResult(eval(query))
+        }else{
+          if(query === '00'){
+              setQuery(value)
+          }else{
+              setQuery(query + value)
+          }
+      }
+      }
+       
     }
   return (
     <div className='container'>
         <div className="overlay"></div>
       <div className="calcCard">
         <div className="display">
-            <div className="query">{query}</div>
+            <div className="query" style={style}>{query}</div>
             {result !== 'null'&& (
                 <div className="result">{result}</div>
             )}
